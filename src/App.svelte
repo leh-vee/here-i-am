28c6f1dd9952409c21c3c0e94db4e140a00d6e93
Levi <levi@the-telling-detail.org>
2022-09-11 14:03:44 -0400

    basic reading ui layout

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+  import TextField from "./lib/TextField.svelte";
+  const couplets = [
+    {
+      piSlice: 3,
+      a: "Here I am a wet, market smile on you absent face still",
+      b: "delivering that neighbourly something to our hood" 
+    },
+    // {
+    //   piSlice: 2,
+    //   a: "Here I am all together chinked by the overhearing",
+    //   b: "of a featherweight junction’s heavy breathing" 
+    // },
+    // {
+    //   piSlice: 1,
+    //   a: "Here I am the other white people, a new Israelite vector",
+    //   b: "starting from the horrible reach of her empty touch" 
+    // }
+  ]
+
+  const currentWordIndex = {
+    couplet: 0,
+    line: 'a',
+    word: '3'
+  }
+  const cwi = currentWordIndex;
+
+  function wordsByLine(coupletIndex, line = 'a') {
+    return couplets[coupletIndex][line].split(' ');
+  }
+</script>
+
+<div class='reader'>
+  <div class='focus'>
+    <h1 class='word'>{ wordsByLine(cwi.couplet, cwi.line)[cwi.word] }</h1>
+  </div>
+  <TextField {couplets} {currentWordIndex} />
+</div>
+
+<style>
+  .reader {
+    height: 100%;
+    display: flex;
+		flex-direction: column;
+    text-align: center;
+  }
+  
+  .focus {
+    flex-grow: 1;
+  }
+
+  .word {
+    font-size: 100px;
+    margin-top: 50%;
+  }
+
+</style>
---
+++ src/lib/Couplet.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let text;
+
+  function wordsByLine(line) {
+    return line.split(' ');
+  }
+</script>
+
+<div class='couplet'>
+  <div class='a-line'>
+    {#each wordsByLine(text.a) as word}
+      <span class='marker'></span>
+    {/each}
+  </div>
+  <div class='b-line'>
+    {#each wordsByLine(text.b) as word}
+      <span class='marker'></span>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .couplet {
+    margin: 10px auto;
+    height: 80%;
+  }
+
+  .couplet .marker {
+    background-color: black;
+    opacity: 0.3;
+    border-radius: 50%;
+    display: inline-block;
+    margin: 0 4px;
+    height: 10px;
+    width: 10px;
+  }
+</style>
