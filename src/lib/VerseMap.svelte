9c2d6a3ce27553295dbe965901654c990d2630a0
Levi <levi@the-telling-detail.org>
2022-09-25 13:17:10 -0400

    refactor verse map

--- src/lib/VerseMap.svelte
+++ src/lib/VerseMap.svelte
@@ -1,28 +1,8 @@
 <script>
-  import { currentVerse } from '../store.js';
+  import LineMap from './LineMap.svelte';
 </script>
 
 <div class='couplet'>
-  <div class='a-line'>
-    {#each $currentVerse['a'] as word}
-      <span class='marker'></span>
-    {/each}
-  </div>
-  <div class='b-line'>
-    {#each $currentVerse['b'] as word}
-      <span class='marker'></span>
-    {/each}
-  </div>
+  <LineMap verseLine={'a'} />
+  <LineMap verseLine={'b'} />
 </div>
-
-<style>
-  .marker {
-    background-color: black;
-    opacity: 0.3;
-    border-radius: 50%;
-    display: inline-block;
-    margin: 0 4px;
-    height: 10px;
-    width: 10px;
-  }
-</style>
