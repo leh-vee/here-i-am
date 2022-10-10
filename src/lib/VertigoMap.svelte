8052694e9956655253f00b82f84d2f8e59dee138
Levi <levi@the-telling-detail.org>
2022-10-10 16:53:17 -0400

    spike: draw centrelines within the bounds of the porjection

--- src/lib/VertigoMap.svelte
+++ src/lib/VertigoMap.svelte
@@ -17,14 +17,17 @@
       .context(ctx);
 
     mercProjection.translate([mapWidth / 2, mapHeight / 2])
-    mercProjection.scale(2800000);
+    mercProjection.scale(2700000);
     const highParkAndHumberside = [ -79.466850201826219, 43.657227646269199 ];
     mercProjection.center(highParkAndHumberside);
+    
+    mercProjection.clipExtent([[0,0], [mapWidth, mapHeight]]);
 
     d3.json("/toronto-centreline-simple.geojson").then(blocks => {
       CentrelineAnimator.blocksGeoJson = blocks;
       let animator = new CentrelineAnimator(ctx, mercProjection);
-      animator.drawBlocksFromNode(13465772);
+      animator.drawAllBlocks(geoGenerator);
+      // animator.drawBlocksFromNode(13465772);
       // ctx.beginPath();
       // geoGenerator({type: 'FeatureCollection', features: blocks.features})
       // ctx.stroke();
