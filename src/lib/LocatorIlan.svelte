c71e4367b834c836f172d9f04660f492361f5187
Levi <levi@the-telling-detail.org>
2023-11-02 14:42:58 -0400

    refactor and style update

--- src/lib/LocatorIlan.svelte
+++ src/lib/LocatorIlan.svelte
@@ -1,22 +1,26 @@
 <script>
   import { onMount } from 'svelte';
-  import { Image, Rect } from 'svelte-konva';
+  import { Image, Rect, Circle } from 'svelte-konva';
   import { geoPath } from "d3";
   import { fetchBlocksForProjection } from '../api/client.js';
   import { projectionForIlan } from '../lib/utils/projections.js';
   import { sefirotPoints } from '../stores/ilan.js';
+  import { currentFromSefirahCoordsGcs } from '../stores/ilan';
 
   let canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
+  const ratio = 6;
 
   $: w = window.innerWidth;  
   $: h = window.innerHeight;  
-  $: ilanWidth = w / 4;
-  $: ilanHeight = h / 4;
+  $: ilanWidth = w / ratio;
+  $: ilanHeight = h / ratio;
   $: locatorPx = { width: ilanWidth, height: ilanHeight };
-  $: margin = Math.max(w, h) / 75;
+  $: margin = Math.max(w, h) / 40;
   $: ilanX = w - ilanWidth - margin;
-  $: ilanY = h - ilanHeight - margin;
+  $: ilanY = margin;
+
+  let markerCoordinatesPx;
 
   onMount(async () => {
     canvas.width = ilanWidth;
@@ -28,6 +32,8 @@
     const projection = await projectionForIlan($sefirotPoints, locatorPx);
     const blocks = await fetchBlocksForProjection(projection, locatorPx);
     console.log('ilan locator map data loaded');
+
+    markerCoordinatesPx = projection($currentFromSefirahCoordsGcs);
 
     let geoGenerator = geoPath().projection(projection).context(ctx);
     ctx.beginPath();
@@ -44,8 +50,8 @@
   width: ilanWidth, 
   height: ilanHeight,
   stroke: 'black',
-  fill: 'white',
-  strokeWidth: 1
+  strokeWidth: 1,
+  fill: 'white'
 }} />
 <Image config={{ 
   image: canvas,
@@ -54,3 +60,12 @@
   width: ilanWidth, 
   height: ilanHeight
 }} />
+{#if markerCoordinatesPx}
+  <Circle config={{
+      x: ilanX + markerCoordinatesPx[0],
+      y: ilanY + markerCoordinatesPx[1],
+      radius: 3,
+      fill: 'black'
+    }}
+  />
+{/if}
