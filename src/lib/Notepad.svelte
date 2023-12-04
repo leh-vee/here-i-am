b7daa06c0a02622eee649e2f7a6c09591128e684
Levi <levi@the-telling-detail.org>
2023-12-04 14:17:26 -0500

    repalce Word with Notepad component

---
+++ src/lib/Notepad.svelte
@@ -0,0 +1,69 @@
+<script>
+    import { Group, Text } from 'svelte-konva';
+    import { currentVerse, wordIndices } from '../stores/text.js';
+
+    let padEl;
+    const padWordEls = { a: [], b: [] };
+
+    $: nWordsLineA = $currentVerse['a'].length;
+    $: nWordsLineB = $currentVerse['b'].length;
+    $: maxWordsPerLine = Math.max(nWordsLineA, nWordsLineB); 
+    $: groupWidth = maxWordsPerLine * window.innerWidth;
+    $: wordBoxMarginA = Math.round(groupWidth / nWordsLineA - window.innerWidth);
+    $: wordBoxMarginB = Math.round(groupWidth / nWordsLineB - window.innerWidth);
+
+    $: isLineA = $wordIndices.line === 'a';
+    $: margin = isLineA ? wordBoxMarginA : wordBoxMarginB;
+    $: xPadPosition =  -$wordIndices.wordIndex * (window.innerWidth + margin);
+    $: yPadPosition =  isLineA ? 0 : -window.innerHeight; 
+
+    $: if (padEl !== undefined) {
+      padEl.to({
+        duration: Math.PI / 10,
+        x: xPadPosition,
+        y: yPadPosition
+      });
+    }
+
+</script>
+
+<Group config={{ 
+  width: groupWidth, 
+  height: window.innerHeight * 2,
+  x: 0, 
+  y: 0,
+  scaleX: 1.0,
+  scaleY: 1,
+}} bind:handle={padEl}>
+  {#each $currentVerse['a'] as word, i}
+    <Text config={{
+      text: word,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      x: i * (window.innerWidth + wordBoxMarginA),
+      align: 'center',
+      verticalAlign: 'middle',
+      fontSize: 50,
+      fontFamily: 'Love Ya Like A Sister',
+      fillEnabled: true,
+      fill: 'gold',
+      strokeWidth: 0,
+    }} bind:handle={padWordEls['a'][i]} />
+  {/each}
+  {#each $currentVerse['b'] as word, i}
+    <Text config={{
+      text: word,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      x: i * (window.innerWidth + wordBoxMarginB),
+      y: window.innerHeight,
+      align: 'center',
+      verticalAlign: 'middle',
+      fontSize: 50,
+      fontFamily: 'Love Ya Like A Sister',
+      fillEnabled: true,
+      fill: 'gold',
+      strokeWidth: 0,
+    }} bind:handle={padWordEls['b'][i]} />
+  {/each}
+</Group>
--- src/lib/VerseExplorer.svelte
+++ src/lib/VerseExplorer.svelte
@@ -2,11 +2,12 @@
   import { Layer } from 'svelte-konva';
   import StreetTraces from './StreetTraces.svelte';
   import SefirahMarker from './SefirahMarker.svelte';
-  import LineMarkers from './LineMarkers.svelte';
   import VerseNumber from './VerseNumber.svelte';
   import { currentChannelFromSefirahCoordsPx, 
     currentChannelToSefirahCoordsPx } from '../stores/treeOfLife.js';
   import Word from './Word.svelte';
+  import LineMarkers from './LineMarkers.svelte';
+  import Notepad from './Notepad.svelte';
   import Ilan from './Ilan.svelte';
   import { wordIndices, currentVerse } from '../stores/text.js';
 
@@ -53,14 +54,41 @@
   {#if isFromEllipsis }
     <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
   {/if}
-  <Word isEllipsis={ isEllipsis } isVisible={!isCountdown && !isSummary} 
+  {#if !isCountdown && !isSummary}
+    <Notepad />
+  {/if}
+  <!-- <Word isEllipsis={ isEllipsis } isVisible={!isCountdown && !isSummary} 
     on:coda={codaSequence} on:nextMovement={ () => { currentMovementIndex += 1 } } 
-    isFlight={isFlight} />
+    isFlight={isFlight} /> 
   {#if isFlight || isRecall}
     <LineMarkers words={ $currentVerse['a'] } line={'a'} />
     <LineMarkers words={ $currentVerse['b'] } line={'b'} />
-  {/if}
+  {/if} -->
   {#if isToEllipsis }
     <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } />
   {/if}
 </Layer>
+<div class='controller'>
+  <span class='back button'
+    on:click={ wordIndices.previousWord } ></span>
+  <span class='forward button'
+    on:click={ wordIndices.nextWord } ></span>
+</div>
+
+<style>
+  .controller {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 25%;
+    display: flex;
+    z-index: 1;
+  }
+
+  .controller .button {
+    width: 50%;
+    text-align: center;
+    vertical-align: baseline;
+    font-size: 5vw;
+  }
+</style>
