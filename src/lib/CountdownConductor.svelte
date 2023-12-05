2144f65b07cc570af1af48cd4f0b952537f557e0
Levi <levi@the-telling-detail.org>
2023-12-05 14:10:52 -0500

    modularize CountdownConductor

--- src/lib/CountdownConductor.svelte
+++ src/lib/CountdownConductor.svelte
@@ -1,14 +1,9 @@
 <script>
   import { Layer } from 'svelte-konva';
-  import StreetTraces from './StreetTraces.svelte';
-  import SefirahMarker from './SefirahMarker.svelte';
   import VerseNumber from './VerseNumber.svelte';
-  import { currentChannelFromSefirahCoordsPx, 
-    currentChannelToSefirahCoordsPx } from '../stores/treeOfLife.js';
-  import Notepad from './Notepad.svelte';
-  import VerseMap from './VerseMap.svelte';
   import Ilan from './Ilan.svelte';
-  import { wordIndices, currentVerseIndex } from '../stores/text.js';
+  import { wordIndices } from '../stores/text.js';
+  import VerseExplorer from './VerseExplorer.svelte';
 
   const movements = ['summary', 'countdown', 'fromEllipsis', 'flight', 'recall', 'toEllipsis'];
   let currentMovementIndex = 0;
@@ -49,18 +44,7 @@
   <Ilan go={ isSummary } />
 </Layer>
 <Layer config={{ visible: !isCountdown && !isSummary }} >
-  <StreetTraces />
-  <!-- {#if isFromEllipsis }
-    <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
-  {/if} -->
-  {#if !isCountdown && !isSummary}
-    {#key $currentVerseIndex}
-      <Notepad /> 
-      <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
-      <VerseMap />
-      <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } />
-    {/key}
-  {/if}
+  <VerseExplorer />
   <!-- <Word isEllipsis={ isEllipsis } isVisible={!isCountdown && !isSummary} 
     on:coda={codaSequence} on:nextMovement={ () => { currentMovementIndex += 1 } } 
     isFlight={isFlight} /> 
@@ -71,27 +55,3 @@
     <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } />
   {/if} -->
 </Layer>
-<div class='controller'>
-  <span class='back button'
-    on:click={ wordIndices.previousWord } ></span>
-  <span class='forward button'
-    on:click={ wordIndices.nextWord } ></span>
-</div>
-
-<style>
-  .controller {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 25%;
-    display: flex;
-    z-index: 1;
-  }
-
-  .controller .button {
-    width: 50%;
-    text-align: center;
-    vertical-align: baseline;
-    font-size: 5vw;
-  }
-</style>
---
+++ src/lib/VerseExplorer.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Notepad from './Notepad.svelte';
+  import VerseMap from './VerseMap.svelte';
+  import SefirahMarker from './SefirahMarker.svelte';
+  import StreetTraces from './StreetTraces.svelte';
+  import { currentChannelFromSefirahCoordsPx, 
+    currentChannelToSefirahCoordsPx } from '../stores/treeOfLife.js';
+  import { currentVerseIndex, wordIndices } from '../stores/text.js';
+</script>
+
+{#key $currentVerseIndex}
+  <StreetTraces />
+  <Notepad /> 
+  <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
+  <VerseMap />
+  <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } />
+{/key}
+<div class='controller'>
+  <span class='back button'
+    on:click={ wordIndices.previousWord } ></span>
+  <span class='forward button'
+    on:click={ wordIndices.nextWord } ></span>
+</div>
+
+<style>
+  .controller {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 25%;
+    display: flex;
+    z-index: 1;
+  }
+
+  .controller .button {
+    width: 50%;
+    text-align: center;
+    vertical-align: baseline;
+    font-size: 5vw;
+  }
+</style>
