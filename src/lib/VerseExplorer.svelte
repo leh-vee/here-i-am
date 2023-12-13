39b09e4d3710bde559766b1e54487dfaebf3d864
Levi <levi@the-telling-detail.org>
2023-12-13 09:20:47 -0500

    add swipe nav to verse explorer

--- src/lib/VerseExplorer.svelte
+++ src/lib/VerseExplorer.svelte
@@ -1,15 +1,17 @@
 <script>
-  import { Layer } from 'svelte-konva';
+  import { Stage, Layer } from 'svelte-konva';
   import Punctuation from './Punctuation.svelte';
   import Notepad from './Notepad.svelte';
   import VerseMap from './VerseMap.svelte';
   import SefirahMarker from './SefirahMarker.svelte';
   import StreetTraces from './StreetTraces.svelte';
   import { currentChannelFromSefirahCoordsPx, 
-    currentChannelToSefirahCoordsPx, blocksForCurrentChannel } from '../stores/treeOfLife.js';
+    currentChannelToSefirahCoordsPx, blocksForCurrentChannel } from '../stores/treeOfLife';
   import { currentVerseIndex, wordIndices, isPunctuationNext, 
-    isCaesura, isEllipsis, isFirstVerseWord, isLastVerseWord } from '../stores/text.js';
+    isCaesura, isEllipsis, isFirstVerseWord, isLastVerseWord } from '../stores/text';
   import Ellipsis from './Ellipsis.svelte';
+  import { swipe } from 'svelte-gestures';
+
 
   $: isPreVerseElliptical = $isEllipsis && $isFirstVerseWord;
   $: isPostVerseElliptical = $isEllipsis && $isLastVerseWord;
@@ -46,31 +48,53 @@
     isCaesura.set(false);
     wordIndices.nextWord();
   }
+
+  function swiped(event) {
+    const direction = event.detail.direction;
+    if (direction === 'left') {
+      nextWord();
+    } else {
+      previousWord();
+    }
+  }
 </script>
 
-<Layer>
-  {#key $currentVerseIndex}
-    <StreetTraces />  
-    {#if $blocksForCurrentChannel}
-      <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
-      <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } 
-        isFromSefirah={false} />
-      {#if $isEllipsis}
-        <Ellipsis />
-      {:else}
-        <VerseMap />
-        <Notepad />
-        <Punctuation on:punctuated={ postPunctuation } />
-      {/if}
-    {/if}
-  {/key}
-</Layer>
+<div id='verse-explorer' use:swipe={{ timeframe: 300, minSwipeDistance: 60 }} 
+  on:swipe={ (e) => { swiped(e) } }>
+  <Stage config={{ width: window.innerWidth, height: window.innerHeight }}>
+    <Layer>
+      {#key $currentVerseIndex}
+        <StreetTraces />  
+        {#if $blocksForCurrentChannel}
+          <SefirahMarker coordsPx={ $currentChannelFromSefirahCoordsPx } />
+          <SefirahMarker coordsPx={ $currentChannelToSefirahCoordsPx } 
+            isFromSefirah={false} />
+          {#if $isEllipsis}
+            <Ellipsis />
+          {:else}
+            <VerseMap />
+            <Notepad />
+            <Punctuation on:punctuated={ postPunctuation } />
+          {/if}
+        {/if}
+      {/key}
+    </Layer>
+  </Stage>
+</div>
 <div class='controller'>
   <span class='back button' on:click={ previousWord } ></span>
   <span class='forward button' on:click={ nextWord } ></span>
 </div>
 
 <style>
+  #verse-explorer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
   .controller {
     position: absolute;
     bottom: 0;
