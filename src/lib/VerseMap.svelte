f4c461170c80bb1f4cc1ab57c80c35559ce8497a
Levi <levi@the-telling-detail.org>
2024-02-05 12:54:13 -0500

    improve precision of word marker coordinates

--- src/lib/VerseMap.svelte
+++ src/lib/VerseMap.svelte
@@ -9,27 +9,33 @@
   $: nSpacesLineA = $currentVerse['a'].length - 1;
   $: nSpacesLineB = $currentVerse['b'].length - 1;
   $: xLineDeltaPx = $currentChannelCoordsPx[1][0] - $currentChannelCoordsPx[0][0] - xMarginPx * 2;
-  $: xSpaceDeltaA = Math.round(xLineDeltaPx / nSpacesLineA);
-  $: xSpaceDeltaB = Math.round(xLineDeltaPx / nSpacesLineB);
+  $: xSpaceDeltaA = xLineDeltaPx / nSpacesLineA;
+  $: xSpaceDeltaB = xLineDeltaPx / nSpacesLineB;
+  $: lineStartX = Math.round(xMarginPx + $currentChannelCoordsPx[0][0]);
+  $: lineEndX = Math.round($currentChannelCoordsPx[1][0] - xMarginPx);
 
-  function xCoordsForBeeAtIndex(index) {
-    let x =  xSpaceDeltaB * index;
-    if ($likePiSlices) x = xLineDeltaPx - x;
+  function xForWordIndexAndLine(index, line='a') {
+    const nWordsInLine = $currentVerse[line].length;
+    const isLineB = line === 'b';
+    const delta = isLineB ? xSpaceDeltaB : xSpaceDeltaA;
+    let x = Math.round(delta * index);
+    if (index === nWordsInLine - 1) x = lineEndX - lineStartX;
+    if ($likePiSlices && isLineB) x = xLineDeltaPx - x;
     return x;
   }
 
 </script>
 
 <Group config={{ 
-  x: xMarginPx + $currentChannelCoordsPx[0][0], 
+  x: lineStartX, 
   y: $currentChannelCoordsPx[0][1]
 }}>
   {#each $currentVerse['a'] as _, i}
-    <WordMarker x={xSpaceDeltaA * i} y={-yOffsetPx} 
+    <WordMarker x={ xForWordIndexAndLine(i) } y={ -yOffsetPx } 
       wordId={`${$currentVerseIndex}-a-${i}`} />
   {/each}
   {#each $currentVerse['b'] as _, i}
-    <WordMarker x={xCoordsForBeeAtIndex(i)} y={yOffsetPx}
+    <WordMarker x={ xForWordIndexAndLine(i, 'b') } y={yOffsetPx}
       wordId={`${$currentVerseIndex}-b-${i}`} />
   {/each}
 </Group>
