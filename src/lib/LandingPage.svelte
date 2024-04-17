d86d65da75353d00bc6694aa0db5f73d08825e67
Levi <levi@the-telling-detail.org>
2024-04-17 16:05:19 -0400

    add title to landing page

--- src/lib/LandingPage.svelte
+++ src/lib/LandingPage.svelte
@@ -10,6 +10,8 @@
 </script>
 
 <div id='landing-page'>
+  <h1 class='title'>Here I Am</h1>
+  <h5 class='sub title'>Our Fallen Gang</h5>
   <button id='go' on:click={dispatchGo}>
     GO
   </button>
@@ -19,12 +21,26 @@
   div#landing-page {
     width: 100%;
     height: 100%;
+    color: white;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+  .title {
+    margin: 0;
+    font-size: 15svw;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  .sub.title {
+    font-size: 5svw;
   }
 
   #go {
     height: fit-content;
+    margin: 30px;
+    padding: 12px;
+    border-radius: 5px;
   }
 </style>
