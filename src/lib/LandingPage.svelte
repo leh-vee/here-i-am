6ed9e8fe2d41a942d8668d70e770be176ace32dd
Levi <levi@the-telling-detail.org>
2024-04-23 18:34:52 -0400

    use konva for landing page elements

--- src/lib/LandingPage.svelte
+++ src/lib/LandingPage.svelte
@@ -1,40 +1,113 @@
 <script>
+  import { Stage, Layer, Circle, Text } from 'svelte-konva';
   import { createEventDispatcher } from 'svelte';
-
   const dispatch = createEventDispatcher();
-  const btnDiameter = Math.round(window.innerWidth * 0.7);
 
+  const xCentre = window.innerWidth / 2;
+  const yCentre = window.innerHeight / 2;
+  const buttonRadius = Math.round(xCentre * 0.7);
+  const diagonalRadius = Math.hypot(xCentre, yCentre);
+
+  let button;
+  let stroke = 'dimgrey';
+  let fill = 'gold';
+  let fontSize = Math.round(window.innerWidth / 5);
+
+  const title = "Here\nI Am";
+  const subtitle = "Our\nFallen\nGang";
+  let titleText = title;
+  let isTitleVisible = true;
   let isTitleFontLoaded = false;
+  
   document.fonts.load("12px Love Ya Like A Sister", "a").then(() =>{
     isTitleFontLoaded = true;
   });
+  
+  function btnPressed() {
+    console.log('button pressed');
+    titleText = subtitle;
+    fill = 'dimgrey';
+    stroke = 'gold';
+    fontSize = Math.round(window.innerWidth / 10)
+  } 
 
-  function dispatchGo() {
-    dispatch('go');
-  }
+  function btnDepressed() { 
+    console.log('button depressed');
+    isTitleVisible = false;
+    button.to({
+      duration: Math.PI / 10,
+      radius: diagonalRadius,
+      onFinish: () => { dispatch('go') }
+    });
+  } 
 </script>
 
 <div id='landing-page'>
-  <button on:click={dispatchGo} id="go" style='--diameter:{btnDiameter};'>
-    {#if isTitleFontLoaded}
-      <span class="title">Here</span>
-      <span class="title">I Am</span>
-    {/if}
-  </button>
+  <Stage config={{ width: window.innerWidth, height: window.innerHeight }}>
+    <Layer>
+      <Circle config={{
+        x: xCentre,
+        y: yCentre,
+        fill: 'black',
+        radius: buttonRadius,
+        strokeWidth: 6,
+        stroke
+      }} 
+      on:pointerdown={ btnPressed }
+      on:pointerup={ btnDepressed }
+      bind:handle={ button } />
+      {#if isTitleFontLoaded && isTitleVisible} 
+        <Text config={{
+          x: 0,
+          y: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+          align: 'center',
+          verticalAlign: 'middle',
+          text: titleText,
+          fontFamily: 'Love Ya Like A Sister',
+          fontSize,
+          fill
+        }} 
+        on:pointerdown={ btnPressed }
+        on:pointerup={ btnDepressed }
+        />
+      {/if}
+    </Layer>
+  </Stage>
 </div>
 
 <style>
   div#landing-page {
     width: 100%;
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background-image: url("/toronto-lines.png");
     background-size: cover;
   }
+
+  /* #title-wrapper {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
+  }
+
+  .title {
+    font-family: 'Love Ya Like A Sister';
+    font-size: 8lvh;
+    color: gold;
+    font-weight: bold;
+    text-decoration: none;z-index: 2;
+  } */
+
+  /* .sub.title {
+    font-size: 4lvh;
+    color: dimgrey;
+  } */
   
-  button#go {
+  /* button#go {
     background: black;
     text-align: center;
     width: calc( var(--diameter) * 1px );
@@ -42,15 +115,14 @@
     border-radius: 100%;
     display: inline-flex;
     flex-direction: column;
+    flex-grow: 0;
     justify-content: center;
     align-items: center;
-  }
-
-  .title {
-    font-family: 'Love Ya Like A Sister';
-    font-size: 10lvh;
-    color: gold;
-    font-weight: bold;
-    text-decoration: none
+    transition:
+    width calc( var(--transDuration) * 1s) ease-in,
+    height calc( var(--transDuration) * 1s) ease-in;
+    user-select: none;
+    --webkit-touch-callout: none;
+    --webkit-tap-highlight-color: rgba(0,0,0,0);
   }
 </style>
