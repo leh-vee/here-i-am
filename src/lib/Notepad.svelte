78f791fe230573f38488345e88ab45c298165598
Levi <levi@the-telling-detail.org>
2024-08-27 16:50:35 -0400

    notepade movement speed should be function of distance between words

--- src/lib/Notepad.svelte
+++ src/lib/Notepad.svelte
@@ -1,86 +1,90 @@
 <script>
-    import { Group, Text } from 'svelte-konva';
-    import { currentVerse, wordIndices, isLineBreak, currentWordId,
+  import { Group, Text } from 'svelte-konva';
+  import { currentVerse, wordIndices, currentWordId,
       currentVerseIndex, isCaesura, isInBetweenWords, isLineA, likePiSlices } from '../stores/text.js';
       
-    export let visible = false;
-    let padEl;
-    let stashedWordEl = null;
-    const wordAttrs = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-      align: 'center',
-      verticalAlign: 'middle',
-      fontSize: Math.round(window.innerWidth / 9),
-      fontFamily: 'Love Ya Like A Sister',
-      fillEnabled: true,
-      fill: 'gold',
-      strokeWidth: 0
-    }
+  export let visible = false;
+  
+  let stashedWordEl = null;
+  let padEl;
 
-    $: nWordsLineA = $currentVerse['a'].length;
-    $: nWordsLineB = $currentVerse['b'].length;
-    $: maxWordsPerLine = Math.max(nWordsLineA, nWordsLineB); 
-    $: groupWidth = maxWordsPerLine * window.innerWidth;
-    $: wordBoxMarginA = Math.round(groupWidth / nWordsLineA - window.innerWidth);
-    $: wordBoxMarginB = Math.round(groupWidth / nWordsLineB - window.innerWidth);
+  const wordAttrs = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    align: 'center',
+    verticalAlign: 'middle',
+    fontSize: Math.round(window.innerWidth / 9),
+    fontFamily: 'Love Ya Like A Sister',
+    fillEnabled: true,
+    fill: 'gold',
+    strokeWidth: 0
+  }
 
-    $: isLineBAndLikeSlices = !$isLineA && $likePiSlices;
-    $: margin = $isLineA ? wordBoxMarginA : wordBoxMarginB;
-    $: textElWidth = (window.innerWidth + margin);
-    $: xPadPosition =  isLineBAndLikeSlices ? ($wordIndices.wordIndex - (nWordsLineB - 1)) * textElWidth : -$wordIndices.wordIndex * textElWidth;
-    $: yPadPosition =  $isLineA ? 0 : -window.innerHeight; 
+  $: nWordsLineA = $currentVerse['a'].length;
+  $: nWordsLineB = $currentVerse['b'].length;
+  $: maxWordsPerLine = Math.max(nWordsLineA, nWordsLineB); 
+  $: groupWidth = maxWordsPerLine * window.innerWidth;
+  $: wordBoxMarginA = Math.round(groupWidth / nWordsLineA - window.innerWidth);
+  $: wordBoxMarginB = Math.round(groupWidth / nWordsLineB - window.innerWidth);
 
-    $: lineBWords = $likePiSlices ? structuredClone($currentVerse['b']).reverse() : $currentVerse['b'];
-    
-    const wordPanDuration = Math.PI / 10;
-    const lineBreakPanDuration = Math.PI / 2;
-    $: if (visible) {
-      const d = ($isLineBreak && !$likePiSlices) ? lineBreakPanDuration : wordPanDuration;
-      movePadToCurrentWord(xPadPosition, yPadPosition, d);
-    }
+  $: isLineBAndLikeSlices = !$isLineA && $likePiSlices;
+  $: margin = $isLineA ? wordBoxMarginA : wordBoxMarginB;
+  $: textElWidth = (window.innerWidth + margin);
+  $: xPadPosition =  isLineBAndLikeSlices ? ($wordIndices.wordIndex - (nWordsLineB - 1)) * textElWidth : -$wordIndices.wordIndex * textElWidth;
+  $: yPadPosition =  $isLineA ? 0 : -window.innerHeight; 
 
-    function movePadToCurrentWord(x, y, duration) {
-      if (padEl !== undefined) {
-        isInBetweenWords.set(true);
-        padEl.to({ 
-          duration, x, y,
-          onFinish: () => {
-            if (stashedWordEl !== null) unstashWord();
-            isInBetweenWords.set(false);
-          }
-        });
-      }
-    }
+  $: lineBWords = $likePiSlices ? structuredClone($currentVerse['b']).reverse() : $currentVerse['b'];
 
-    $: if ($isCaesura) stashWord();
+  let xPositionWidth;
+  $: if (padEl !== undefined) xPositionWidth = padEl.width() - textElWidth;
+  $: if (visible) {
+    const xCurrentPosition = padEl.position()['x'];
+    const xDistance = Math.abs(xCurrentPosition - xPadPosition);
+    const xDistancePercent = xDistance / xPositionWidth;
+    movePadToCurrentWord(xPadPosition, yPadPosition, xDistancePercent);
+  } 
 
-    function stashWord() {
-      const duration = Math.PI / 10;
-      stashedWordEl = padEl.findOne(`#${$currentWordId}`);
-      const currentX = stashedWordEl.attrs.x;
-      const x = isLineBAndLikeSlices ? currentX + textElWidth : currentX - textElWidth;
-      stashedWordEl.to({ 
-        duration, x,
+  function movePadToCurrentWord(x, y, duration) {
+    if (padEl !== undefined) {
+      isInBetweenWords.set(true);
+      padEl.to({ 
+        duration, x, y,
         onFinish: () => {
-          stashedWordEl.visible(false);
-        } 
+          if (stashedWordEl !== null) unstashWord();
+          isInBetweenWords.set(false);
+        }
       });
-    }  
-    
-    function unstashWord() {
-      stashedWordEl.visible(true);
-      stashedWordEl = null;
     }
+  }
 
-    function wordId(i, line='b') {
-      let id = `${$currentVerseIndex}-${line}-${i}`;
-      if (line === 'b' && $likePiSlices) {
-        const reverseIndex = nWordsLineB - 1 - i; 
-        id = `${$currentVerseIndex}-${line}-${reverseIndex}`
-      }
-      return id;
+  $: if ($isCaesura) stashWord();
+
+  function stashWord() {
+    const duration = Math.PI / 10;
+    stashedWordEl = padEl.findOne(`#${$currentWordId}`);
+    const currentX = stashedWordEl.attrs.x;
+    const x = isLineBAndLikeSlices ? currentX + textElWidth : currentX - textElWidth;
+    stashedWordEl.to({ 
+      duration, x,
+      onFinish: () => {
+        stashedWordEl.visible(false);
+      } 
+    });
+  }  
+  
+  function unstashWord() {
+    stashedWordEl.visible(true);
+    stashedWordEl = null;
+  }
+
+  function wordId(i, line='b') {
+    let id = `${$currentVerseIndex}-${line}-${i}`;
+    if (line === 'b' && $likePiSlices) {
+      const reverseIndex = nWordsLineB - 1 - i; 
+      id = `${$currentVerseIndex}-${line}-${reverseIndex}`
     }
+    return id;
+  }
 </script>
 
 <Group config={{ 
