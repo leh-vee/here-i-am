927088e04d35db39eff47c72d754a3d380cede87
Levi <levi@the-telling-detail.org>
2024-12-18 16:20:09 -0500

    use classic font for Game Over text

--- src/lib/GameOver.svelte
+++ src/lib/GameOver.svelte
@@ -14,11 +14,11 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: xx-large;
   }
   h5 {
     margin: 0;
     color: gold;
+    font-family: "Press Start 2P", serif;
+    font-size: 8vw;
   }
 </style>
