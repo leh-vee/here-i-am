acdbe56048919f0ec1d6481653208d1e9caf1f2d
Levi <levi@the-telling-detail.org>
2024-12-27 19:30:48 -0500

    first pass at making faq text convo

--- src/lib/menus/InfoDropDownMenu.svelte
+++ src/lib/menus/InfoDropDownMenu.svelte
@@ -6,18 +6,88 @@
   export let isVisible = false;
 
   let faqs = [];
+  let nFaqs;
   fetchFaqText().then(lines => {
     faqs = serializeCouplets(lines);
+    nFaqs = faqs.length;
   });
+
+  let faqIndex = 1;
+  let hasQuestionArrived = false;
+  let hasAnswerArrived = false;
+
+  $: if (faqIndex) {
+    setTimeout(() => {
+      hasQuestionArrived = true;
+    }, getRandomTextInterval());
+  }
+
+  $: if (hasQuestionArrived) {
+    setTimeout(() => {
+      hasAnswerArrived = true;
+    }, getRandomTextInterval());
+  }
+
+  $: if (hasAnswerArrived) {
+    setTimeout(nextQuestion, 1000);
+  }
+   
+  function nextQuestion() {
+    if (faqIndex < nFaqs - 1) {
+      faqIndex += 1;
+      hasQuestionArrived = false;
+      hasAnswerArrived = false;
+    }
+  }
+
+  const flashingDotsReset = [true, false, false]
+  let flashingDots = flashingDotsReset;
+
+  setInterval(() => {
+    if (flashingDots[0]) {
+      flashingDots = [false, true, false];
+    } else if (flashingDots[1]) {
+      flashingDots = [false, false, true];
+    } else {
+      flashingDots = flashingDotsReset
+    }
+  }, 600);
+
+  function getRandomTextInterval() {
+    const seconds = Math.random() * (10 - 5) + 5;
+    return seconds * 1000;
+  }
+
 </script>
 
 <DropDownMenu isHidden={ !isVisible }>
   <div id='info'>
     <h3>Frequently Asked Questions</h3>
     <div id='faq'>
-      {#each faqs as qaPair}
-        <span class='question'>{ qaPair.a }</span>
-        <span class='answer'>{ qaPair.b }</span>
+      {#each faqs.slice(0, faqIndex) as qaPair, i}
+        {#if i === faqIndex - 1}
+          {#if hasQuestionArrived}
+            <p class='question'>{ qaPair.a }</p>
+          {:else}
+            <p class='waiting question'>
+              {#each flashingDots as isFlashing}
+                <span class='dot' class:flash={isFlashing}>&#x2022;</span>
+              {/each}
+            </p>
+          {/if}
+          {#if hasAnswerArrived} 
+            <p class='answer'>{ qaPair.b }</p>
+          {:else if hasQuestionArrived}
+            <p class='waiting answer'>
+              {#each flashingDots as isFlashing}
+                <span class='dot' class:flash={isFlashing}>&#x2022;</span>
+              {/each}
+            </p>
+          {/if}
+        {:else}
+          <p class='question'>{ qaPair.a }</p>
+          <p class='answer'>{ qaPair.b }</p>
+        {/if}
       {/each}
     </div>
     
@@ -42,9 +112,10 @@
     display: inline-flex;
     flex-direction: column;
     justify-content: space-evenly;
+    width: 100%;
   }
   
-  span {
+  p {
     font-family: Helvetica, sans-serif;
     font-size: 3.5vw;
     font-weight: 400;
@@ -57,10 +128,24 @@
     border-radius:10px;
   }
 
-  .answer {
+  p.answer {
     align-self: flex-end;
     background-color: rgb(41, 132, 245);
     color: white;
+  }
+
+  p.waiting {
+    padding: 0 5px;
+  }
+  
+  .dot {
+    font-size: 2.5vh;
+    color: white;
+    transition: color 0.7s ease-in-out;
+  }
+
+  .dot.flash {
+    color: darkgrey;
   }
 
 </style>
