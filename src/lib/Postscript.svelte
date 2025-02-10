a4ccb2b1f69a02f13610cde59116911b30a21543
Levi <levi@the-telling-detail.org>
2025-02-10 18:01:41 -0500

    a little transition goes a long way

--- src/lib/Postscript.svelte
+++ src/lib/Postscript.svelte
@@ -30,21 +30,19 @@
     <div class='pane'>
       <HighPiScores />
     </div>
+  </div> 
+  <div class='footer' class:visible={ isGameOverReady }>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span class='bullet' class:selected={ isGameOverScreen }
+      on:click={ () => { isGameOverScreen = true } }>
+      &bull;
+    </span>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span class='bullet' class:selected={ isHighScoreScreen }
+      on:click={ () => { isGameOverScreen = false } }>
+      &bull;
+    </span>
   </div>
-  {#if isGameOverReady} 
-    <div id='footer'>
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <span class='bullet' class:selected={ isGameOverScreen }
-        on:click={ () => { isGameOverScreen = true } }>
-        &bull;
-      </span>
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <span class='bullet' class:selected={ isHighScoreScreen }
-        on:click={ () => { isGameOverScreen = false } }>
-        &bull;
-      </span>
-    </div>
-  {/if}
 </div>
 
 <style>
@@ -70,13 +68,18 @@
     width: 100%;
   }
 
-  #footer {
+  .footer {
     position: absolute;
     bottom: 2dvh;
     left: 0;
     right: 0;
     color: white;
     text-align: center;
+    font-size: 0dvh;
+    transition: font-size 1s ease-in-out 500ms;
+  }
+
+  .footer.visible {
     font-size: 5dvh;
   }
 
