2438c69fc4c9b6cdf9672dce3a28e13a4b3ab0ab
Levi <levi@the-telling-detail.org>
2025-02-10 17:27:08 -0500

    leader board fonts and styles

--- src/lib/HighPiScores.svelte
+++ src/lib/HighPiScores.svelte
@@ -1,82 +1,103 @@
 <script>
+  const leaders = [
+    {
+      name: 'Richard',
+      time: '13:07.956',
+      date: '04/02/25'
+    },
+    {
+      name: 'Tobias',
+      time: '25:21.112',
+      date: '03/01/25'
+    },
+    {
+      name: 'Maya',
+      time: '31:21.654',
+      date: '21/11/24'
+    }
+  ]
 
 </script>
 
 <div id='high-scores'>
-  <div class='title'>
-    <h2>Masters of <span class='pi'>π</span></h2>
-  </div>
-  <div class='list'>
-    <table>
-      <tbody>
-        <tr>
-          <td>Anastasia</td>
-          <td>14:35.956</td>
-        </tr>
-        <tr>
-          <td>Dara</td>
-          <td>17:21.731</td>
-        </tr>
-        <tr>
-          <td>Mickey</td>
-          <td>20:44.739</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <h1 id='title'>Masters of <span id='pies'>🥧🥧🥧</span></h1>
+  <h5 id='subtitle'><sup>*</sup>those quickest to score 3x</h5>
+  {#each leaders as leader, i}
+    <div class='leader'>
+      <h1 class='name'>{ leader.name }</h1>
+      <h1 class='time'>{ leader.time }</h1>
+      <h1 class='date'>{ leader.date }</h1>
+    </div>
+  {/each}
 </div>
 
 <style>
   #high-scores {
     width: 100%;
     height: 100%;
-    font-family: Helvetica, sans-serif;
-  } 
-
-  .title {
-    width: 100%;
-    height: 30%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
+  } 
+
+  #high-scores #title {
+    margin: 0;
+    font-family: 'Press Start 2P';
+    font-size: 2.5dvh;
+    color: lightgoldenrodyellow;
   }
 
-  .title h2 {
-    color: gold;
-    font-size: 4.5dvh;
-  }
-
-  .pi {
-    font-family: Times New Roman; 
-    font-size: 7dvh;
-  }
-
-  .list {
-    width: 100%;
-    height: 70%;
-  }
-
-  table {
-    table-layout: fixed;
-    width: 100%;
-    font-size: 6vw;
-  }
-
-  tr {
-    height: 40px;
-    vertical-align: middle;
-  }
-
-  tr td:first-child {
-    color: lightgray;
-    text-align: right;
-    padding-right: 5%;
+  #high-scores #subtitle {
+    margin-top: 0;
+    margin-bottom: 10dvw;
+    font-size: 1.5dvh;
+    font-family: monospace;
+    color: white;
   }
   
-  tr td:last-child {
+  #title #pies {
+    font-size: 5dvh;
+  } 
+
+  #high-scores .leader {
+    text-align: center;
+    margin-bottom: 10dvw;
+  }
+  
+  .leader h1 {
+    margin-top: 0;
+    margin-bottom: 2dvw;
+  }
+
+  .leader .name {
+    font-size: 4dvh;
+    font-family: 'Love Ya Like A Sister';
+  } 
+
+  .leader:nth-of-type(1) .name {
+    color: gold;
+  }
+
+  .leader:nth-of-type(2) .name {
+    color: silver;
+  }
+
+  .leader:nth-of-type(3) .name {
+    color: #FF5733;
+  }
+
+  .leader .time {
+    font-size: 3dvh;
     color: white;
     font-family: 'digital';
-    text-align: center;
+  }
+
+  .leader .date {
+    font-family: 'Homemade Apple';
+    font-size: 1.5dvh;
+    margin: 0;
+    color: lightgray;
   }
 
 </style>
