0080d22bbdb23c0e622e0556a07e2fad6459811f
Levi <levi@the-telling-detail.org>
2025-02-10 19:13:54 -0500

    update leaderboard subtitle

--- src/lib/HighPiScores.svelte
+++ src/lib/HighPiScores.svelte
@@ -21,7 +21,7 @@
 
 <div id='high-scores'>
   <h1 id='title'>Masters of <span id='pie'>🥧</span></h1>
-  <h5 id='subtitle'><sup>*</sup>those quickest to score 3x</h5>
+  <h5 id='subtitle'>(those quickest to score a hat trick)</h5>
   {#each leaders as leader, i}
     <div class='leader'>
       <h1 class='name'>{ leader.name }</h1>
