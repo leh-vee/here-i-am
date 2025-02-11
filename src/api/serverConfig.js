536d5f2c6e0de3f926a70da7410365075f8a5012
Levi <levi@the-telling-detail.org>
2025-02-11 16:35:16 -0500

    add retry logic to data fetch

--- src/api/serverConfig.js
+++ src/api/serverConfig.js
@@ -2,11 +2,30 @@ const HOST = import.meta.env.VITE_HOST_URL;
 const API_BASE_URL = `${HOST}/here-i-am/data`;
 
 
-export async function fetchData(endpoint, options = {}) {
-  const url = `${API_BASE_URL}/${endpoint}/`;
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
+async function fetchWithRetry(url, options, retries, delay = 3000) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      if (!navigator.onLine) {
+        await new Promise(resolve => {
+          window.addEventListener('online', resolve, { once: true });
+        });
+      }
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    } catch (error) {
+      if (i < retries - 1) {
+        await new Promise(resolve => setTimeout(resolve, delay));
+      } else {
+        throw error;
+      }
+    }
   }
-  return response.json();
+}
+
+export async function fetchData(endpoint, options = {}, retries = 3) {
+  const url = `${API_BASE_URL}/${endpoint}/`;
+  return fetchWithRetry(url, options, retries);
 }
